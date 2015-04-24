70a9b9a2f81c94310ce2727706720c96c903c389
Jared Wright <jtw57@gmail.com>
2015-04-24 13:31:37 -0400

    initial video upload end to end

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var request = require('request');
 var routes = require('./routes/index');
 var redis = require('redis');
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
+var ffmpeg = require('fluent-ffmpeg');
 
 var app = express();
 
@@ -27,35 +30,78 @@ app.use(function(req, res, next) {
 
 app.use(logger('dev'));
 app.use(cookieParser());
-app.use(session({ resave: true,
+app.use(session({ resave: false,
                   store: new RedisStore({
                     host: '127.0.0.1',
                     port: 6379,
                     prefix: 'sess'
                   }),
-                  saveUninitialized: true,
+                  saveUninitialized: false,
                   secret: 'fybriseverything' }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(multer({ dest: './uploads/',
     rename: function (fieldname, filename) {
         return filename+Date.now();
+    },
+    onParseStart: function() {
+
+    },
+    onParseEnd: function(req, next) {
+      var data = req.body;
+
+      if(data.episodeName == null || typeof data.episodeName == "undefined") {
+        console.log("No episode name sent")
+        next({error: "No episode name sent"})
+      } else if (data.seriesId == null || typeof data.seriesId == "undefined") {
+        console.log("No series id sent")
+        next({error: "No series id sent"})
+      } 
     },
     onFileUploadStart: function (file, response) {
       console.log(response.body)
       console.log(file.originalname + ' is starting ...')
     },
     onFileUploadComplete: function (file, req, res) {
-      console.log(file)
-      console.log(file.fieldname + ':' + req.session.seriesId + ' uploaded to  ' + file.path)
-      done=true;
+      var episodeName = req.body.episodeName;
+      var seriesId = req.body.seriesId;
+
+      //get length of file w/ ffmpeg
+
+      var command = ffmpeg(file.path);
+      command.ffprobe(0, function(err, data) {
+        console.log(file.path+' metadata:');
+        console.dir(data);
+        var duration = data.streams[0].duration || 0;
+        data = {
+          episodeName: episodeName,
+          seriesId: seriesId,
+          fileName: file.filename,
+          lengthOfFile: duration
+        }
+
+        request.post("http://localhost:5000/videos", data, function(err, data){
+          if (err) console.log(err);
+          console.log(file.fieldname+  ' uploaded to  ' + file.path);
+          done=true;
+          next();
+        })
+        
+      });
+
+      
+
+
+    
     },
     onError: function (error, next) {
       console.log(error)
       next(error)
     }
-    }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+}));
+
 
 app.post('/upload', routes.uploadPOST);
 // development error handler
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 _title = "fybr";
 
 exports.uploadPOST = function(req, res, next) {
-	req.session.seriesId = req.body.seriesId;
-	console.log("Working: "+req.session.seriesId)
-	res.send("{\"message\": \"Uploaded\")");
+
+	res.send("{\"message\": \"Uploading\")");
 }
